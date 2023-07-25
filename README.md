1fb3c14b0aca6b019fc215218398f0aaa9c968dd
Jake McGee <jake.mcgee18@yahoo.co.uk>
2023-07-25 21:13:29 +0100

    Completed code formatting for project. Most requirements fulfilled. Unable to implement Luhn's algorithm check, email sent directly from HTML/java using submission, and the colour format on the validation text do not work as per the javascript validation. Complete more learning on practice on these aspects then return to project to rectify and fix.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # dncauseofcontact
 <h1>Interpreting Business Requirements to Create a Cause of Contact Form Using HTML, CSS & Javascript.</h1>
+<p> Business requirements given, assumptions had to be made before creation. First attempt at test-driven development used to create project.<p>
+<p> I would love to see a working completed code structure that satisfies all conditions - especially for the areas I couldnt work out. PLease let me know/feel free to contribute solutions on my git hub - HTTPS: https://github.com/McGee18/dncontactform.git // SSH: git@github.com:McGee18/dncontactform.git <p>
+<p> Specific requirements included:</p>
 
-<p> Update with the specific requirements tomorrow:</p>
+<ul> 
+<li> Fields to input name, email, credit/debit card number.</li>
+<li> Submission button to submit input.</li>
+<li> Validation checks to be done against each field - First Name Last Name, User@domain, Valid digit length  (luhn's algorithm if possible).</li>
+<li> Notification to appear as soon as invalid submission attempted - error messages used.</li>
+<li> Text colour to change invalid entries based upon required fields - couldnt get to work for name - works for email and card number.</li>
+<li> Design to follow wireframe design provided - colour schemes, text format & page design.</li>
+<li> Submission button to generate email to designated inbox - unable to work out using Javascript and HTML alone - managed to have HTML generate email to send, but not to send automatically or include generated fields.</li>
+</ul>
+
+
--- luhnscheck.js
+++ luhnscheck.js
@@ -1,27 +1,42 @@
-$('#cc').on('input', function(){
-    if (checkLuhn($('#cc').val())) {
-      $('#status').html('Luhn Check: Valid');
-    } else {
-      $('#status').html('Luhn Check: Invalid');
-    }
-  });
+const luhnCheck = num => {
+  let arr = (num + '')
+    .split('')
+    .reverse()
+    .map(x => parseInt(x));
+  let lastDigit = arr.splice(0, 1)[0];
+  let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+  sum += lastDigit;
+  return sum % 10 === 0;
+}
+// unsure how to implement this const as a validation check
 
-function checkLuhn(value) {
-    // remove all non digit characters
-    var value = value.replace(/\D/g, '');
-    var sum = 0;
-    var shouldDouble = false;
-    // loop through values starting at the rightmost side
-    for (var i = value.length - 1; i >= 0; i--) {
-        // parse converts the text string into a number for us in the check - used tel number as credit card number format- figure out what needs to change.
-      var digit = parseInt(value.charAt(i));
+
+
+
+// PHP & java?
+// $('#cc').on('input', function(){
+//     if (checkLuhn($('#cc').val())) {
+//       $('#status').html('Luhn Check: Valid');
+//     } else {
+//       $('#status').html('Luhn Check: Invalid');
+//     }
+//   });
+
+// function checkLuhn(value) {
+//     // remove all non digit characters
+//     var value = value.replace(/\D/g, '');
+//     var sum = 0;
+//     var shouldDouble = false;
+//     // loop through values starting at the rightmost side
+//     for (var i = value.length - 1; i >= 0; i--) {
+//         // parse converts the text string into a number for us in the check - used tel number as credit card number format- figure out what needs to change.
+//       var digit = parseInt(value.charAt(i));
       
-      if (shouldDouble) {
-        if ((digit *= 2) > 9) digit -= 9;
-      }
+//       if (shouldDouble) {
+//         if ((digit *= 2) > 9) digit -= 9;
+//       }
   
-      sum += digit;
-      shouldDouble = !shouldDouble;
-    }
-    return (sum % 10) == 0;
-  }
+//       sum += digit;
+//       shouldDouble = !shouldDouble;
+//     }
+//     return (sum % 10) == 0;
